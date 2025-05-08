aa6aad58001212c0eaf680c6a5bfec2aed70f554
vikas5126 <vikas18046171@gmail.com>
2025-05-08 21:02:37 +0530

    adding cors

--- src/app.ts
+++ src/app.ts
@@ -22,7 +22,7 @@ config({
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.use(cors());
+app.use(cors({origin: 'http://localhost:5173/'}));
 app.use(express.json());
 
 const stripeKey = process.env.STRIPE_KEY || "";
